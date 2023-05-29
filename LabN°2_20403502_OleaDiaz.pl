4e73b15b3fefb017453e3389799fe01be9c0c807
OleaDíaz <bastian.olea@usach.cl>
2023-05-29 17:32:32 -0400

    System, systemAddDrive

--- LabN°2_20403502_OleaDiaz.pl
+++ LabN°2_20403502_OleaDiaz.pl
@@ -15,8 +15,28 @@ TDA's:
 
 % Definición del TDA
 tda_system([Nombre, Timestamp, [], [], [], [], [], []], Nombre, Timestamp).
+tda_drive([Letter, String, Number], Letter, String, Number).
 
-% Ejemplo de uso del TDA
+% No requeridos
+checkLetterRep(List) :-
+    getFirstElement(List, Firsts),
+    sort(Firsts, SortedFirsts),
+    length(Firsts, N),
+    length(SortedFirsts, N).
+    
+getFirstElement([], []).
+getFirstElement([[First|_]|Rest], [First|Firsts]) :-
+    getFirstElement(Rest, Firsts).
+
+
+% Requerimientos funcionales
 system(TDA, Nombre) :-
     get_time(Timestamp),
     tda_system(TDA, Nombre, Timestamp).
+
+systemAddDrive(S1, Letter, String, Number, S2) :-
+    tda_drive(Drive, Letter, String, Number),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
+    append(L3, [Drive], NewDriveList),
+    S2 = [Nombre, Timestamp, L1, L2, NewDriveList, L4, L5, L6],
+    checkLetterRep(NewDriveList).
