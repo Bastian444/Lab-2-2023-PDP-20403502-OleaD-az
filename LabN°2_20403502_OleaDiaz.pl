b8bf7a46126e56161c672c2fe2a97a3ab23cd608
OleaDíaz <bastian.olea@usach.cl>
2023-05-31 13:32:19 -0400

    systemLogin

--- LabN°2_20403502_OleaDiaz.pl
+++ LabN°2_20403502_OleaDiaz.pl
@@ -16,18 +16,24 @@ TDA's:
 % Definición del TDA
 tda_system([Nombre, Timestamp, [], [], [], [], [], []], Nombre, Timestamp).
 tda_drive([Letter, String, Number], Letter, String, Number).
+tda_user(Name, Name).
 
 % No requeridos
+getFirstElement([], []).
+getFirstElement([[First|_]|Rest], [First|Firsts]) :-
+    getFirstElement(Rest, Firsts).
+
 checkLetterRep(List) :-
     getFirstElement(List, Firsts),
     sort(Firsts, SortedFirsts),
     length(Firsts, N),
     length(SortedFirsts, N).
-    
-getFirstElement([], []).
-getFirstElement([[First|_]|Rest], [First|Firsts]) :-
-    getFirstElement(Rest, Firsts).
 
+userRep(String, List) :-
+    \+ (select(String, List, _)),
+    !.
+
+isnull([]).
 
 % Requerimientos funcionales
 system(TDA, Nombre) :-
@@ -40,3 +46,18 @@ systemAddDrive(S1, Letter, String, Number, S2) :-
     append(L3, [Drive], NewDriveList),
     S2 = [Nombre, Timestamp, L1, L2, NewDriveList, L4, L5, L6],
     checkLetterRep(NewDriveList).
+
+systemRegister(S1, User, S2) :-
+    tda_user(NewUser,User),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
+    userRep(NewUser,L2),
+    append(L2, [NewUser], NewUsers),
+    S2 = [Nombre, Timestamp, L1, NewUsers, L3, L4, L5, L6].
+
+systemLogin(S1, User, S2) :-
+    tda_user(NewUser,User),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
+    isnull(L1),
+    member(NewUser,L2),
+    append(L1,[NewUser], NewCurrentUser),
+    S2 = [Nombre, Timestamp, NewCurrentUser, L2, L3, L4, L5, L6].
