7ad1cfee105aab3cf2f3b60929a842c956dc6b4b
OleaDíaz <bastian.olea@usach.cl>
2023-06-10 01:30:45 -0400

    systemCd (Todas sus opciones)

--- LabN°2_20403502_OleaDiaz.pl
+++ LabN°2_20403502_OleaDiaz.pl
@@ -5,7 +5,7 @@ tda_user(Name, Name).
 tda_path([StringPath,User,Drive,_,_],StringPath,User,Drive).
 
 % No requeridos
-get_first_element([String], String).
+get_first_element_string([String], String).
 
 
 getFirstElement([], []).
@@ -22,7 +22,7 @@ userRep(String, List) :-
     \+ (select(String, List, _)),
     !.
 
-isnull([]). % Para preguntar por no null añadir not, es decir, not(isnull(algo))
+isnull([]). % Para preguntar por no null añadir \+, es decir, \+ (isnull(algo))
 
 checkDrive(Letter, Lists) :- % Checkear que sea una letra de un drive, que ya exista
     member([Letter|_], Lists).
@@ -30,7 +30,45 @@ checkDrive(Letter, Lists) :- % Checkear que sea una letra de un drive, que ya ex
 path([StringPath, User, Drive, Timestamp1, Timestamp2], StringPath, User, Drive) :-
     get_time(Timestamp1),
     get_time(Timestamp2),
-    tda_path(_,_,User,Drive).
+    tda_path(_,_,User,Drive). % Crea un path
+
+get_substring(String, Substring) :- % Recorta strings tipo Holamundo/lab1 -> Holamundo
+    split_string(String, "/", "", [Substring|_]).
+
+
+% Para checkear que un string solo contenga Letras y Numeros 
+soloLetrasyNumeros(String) :-
+    string_chars(String, Chars),
+    soloLetrasyNumerosLista(Chars).
+
+soloLetrasyNumerosLista([]).
+soloLetrasyNumerosLista([Char | Rest]) :-
+    char_type(Char, alnum),
+    soloLetrasyNumerosLista(Rest).
+
+% Hecha para el cd de "./folder1"
+puntoSlashFolder(String) :-
+    sub_string(String, 0, 2, _, "./"),
+    sub_string(String, 2, _, 0, Substring),
+    soloLetrasyNumeros(Substring).
+
+% Hecha para "dir1/dir2" -> dir2
+subdir(String, Result) :-
+    split_string(String, "/", "", [_|Result]).
+
+% Hecha para checkear si "dir1/dir2"
+dirSlashdir(S) :-
+    string_codes(S, Codes),
+    maplist([C]>>(C>=48,C=<57;C>=65,C=<90;C>=97,C=<122;C=47), Codes).
+
+% Hecha para "../dir1"
+puntoPuntoSlash(S) :-
+    sub_string(S, 0, 3, _, "../").
+
+% Hecha para checkear si "././././"
+puntosySlashes(S) :-
+    string_codes(S, Codes),
+    maplist([C]>>(C=46;C=47), Codes).
 
 % Requerimientos funcionales
 system(TDA, Nombre) :-
@@ -70,10 +108,60 @@ systemSwitchDrive(S1, Letter, S2) :-
     append([],Letter, NewCurrentDrive), % Resetear el drive
     S2 = [Nombre, Timestamp, L1, L2, L3, NewCurrentDrive, L5, L6].
 
-systemMkdir(S1,StringPath,S2) :-
+systemMkdir(S1, StringPath,S2) :-
     S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
-    not(isnull(L1)), % Checkear que haya un usuario logeado
-    get_first_element(L1,Cosa),
+    \+ (isnull(L1)), % Checkear que haya un usuario logeado
+    get_first_element_string(L1,Cosa),
     path(Path,StringPath,Cosa,L4),
     append(L6,[Path],NewPaths),
     S2 = [Nombre, Timestamp, L1, L2, L3, L4, L5, NewPaths].
+
+% systemCd
+
+systemCd(S1, StringPath, S2) :- % './' - REDY
+    StringPath = "./",
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6].
+
+systemCd(S1, StringPath, S2) :- % '..' - REDY
+    StringPath = "..",
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, [CPath], L6],
+    get_substring(CPath,NewPath),
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, [NewPath], L6].
+
+systemCd(S1, StringPath, S2) :- % '/' - REDY
+    StringPath = "/",
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, _, L6],
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, ["/"], L6].
+
+systemCd(S1, StringPath, S2) :- % 'folder1' - REDY
+    soloLetrasyNumeros(StringPath),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, _, L6],
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, [StringPath], L6].
+
+systemCd(S1, StringPath, S2) :- % '/.folder1' - REDY
+    puntoSlashFolder(StringPath),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, _, L6],
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, [StringPath], L6].
+
+systemCd(S1, StringPath, S2) :- % 'folder/folder11' - REDY
+    dirSlashdir(StringPath),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, _, L6],
+    %subdir(StringPath,NewPath),
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, [StringPath], L6].
+
+% Suponiendo que hay un directorio actual y se llama 'dir1'
+systemCd(S1, StringPath, S2) :- % '.' - REDY
+    StringPath = ".",
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6].
+  
+systemCd(S1, StringPath, S2) :- % '../dir1' - REDY
+    puntoPuntoSlash(StringPath),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6].
+
+systemCd(S1, StringPath, S2) :- % '././././' - Redy
+    puntosySlashes(StringPath),
+    S1 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6],
+    S2 = [Nombre, Timestamp, L1, L2, L3, L4, L5, L6].
