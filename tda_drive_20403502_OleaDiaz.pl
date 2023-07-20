300deb90869fa01582b8ddeb42371f71e830a5dc
OleaDíaz <bastian.olea@usach.cl>
2023-07-20 19:47:35 -0400

    Organización archivos (Comodín)

---
+++ tda_drive_20403502_OleaDiaz.pl
@@ -0,0 +1,6 @@
+:- module(tda_drive_20403502_OleaDiaz, [tda_drive/4]).
+
+% TDA Drive
+% Dominio: Drive x Letra x Nombre x Capacidad
+% Descripción: Crea un drive a parte de los valores ingresados.
+tda_drive([Letter, String, Number], Letter, String, Number).
